8c00a1fd36e5917194d45ac185c2d5397ba2382e
Juan Meza <jcmeza@ucmerced.edu>
2018-05-28 17:30:45 -0400

    updated runbfodisc to run all 10 iterates like runbfo

--- runbfodisc.m
+++ runbfodisc.m
@@ -1,38 +1,42 @@
-%
-% set numerical run
-run = 4;
-%
-% set initial iterate value
-init_x0 = tleed_x0;
-x0 = init_x0(run).x;
-p0  = cell2mat(init_x0(run).p);
-xp = [ p0 x0']; % optimize with respect to both p and (z,x,y)
-xp0 = xp;
-disp(xp0'); % display initial guess
+%% Run BFO with combined discrete/continuous variables
 
-% first 14 variables are discrete; all others continuous
-xdisc  = repmat('i',14,1)';
-xcont = repmat('c',42,1)';
-xtype = strcat(xdisc,xcont);
-%
-% other options
-plower = ones(14,1)';
-pupper = 2*ones(14,1)';
-delta  = 1.0; % possibly use smaller initial box for constraints
-xlower = x0 - delta*max(1.0,abs(x0));
-xupper = x0 + delta*max(1.0,abs(x0));
-xplower = [plower xlower'];
-xpupper = [pupper xupper'];
+for run=1:10
+    disp(run);
+    
+%%  Set initial iterate value
+    init_x0 = tleed_x0;
+    x0 = init_x0(run).x; 
+    p0  = cell2mat(init_x0(run).p);
+    xp0 = [ p0 x0']; % optimize with respect to both p and (z,x,y)
+    disp(xp0'); % display initial guess
 
-fbound = 1.0; % Not used for now
-disp(run);
-[ x, fx, msg, wrn, neval, f_hist] = bfo(@(x)tleedfcn2(x), xp0, ...
-        'epsilon', 0.0001, 'maxeval', 3000, ... 
-        'xlower', xplower, 'xupper', xpupper, ...
-        'save-freq', 10, 'restart-file', 'bfodisc.restart', ...
-        'verbosity', 'low'); %, ...
-        %'f-call-type', 'with-bound', ...
-        %'f-bound', 1.0)
- xstar(run).x = x;
- xstar(run).fx = fx;
+%% First 14 variables are discrete; all others continuous
+    xdisc  = repmat('i',14,1)';
+    xcont = repmat('c',42,1)';
+    xtype = strcat(xdisc,xcont);
+
+%% Other options
+    plower = ones(14,1)'; % Lower/Upper bound for discrete variables
+    pupper = 2*ones(14,1)';
+    delta  = 1.0;         % possibly use smaller initial box for constraints
+    xlower = x0 - delta*max(1.0,abs(x0));
+    xupper = x0 + delta*max(1.0,abs(x0));
+    xplower = [plower xlower']; % combine all bounds
+    xpupper = [pupper xupper'];
+    fbound = 1.0; % Not used for now
+
+%% Call BFO
+    [ x, fx, msg, wrn, neval, f_hist] = bfo(@(x)tleedfcn2(x), xp0, ...
+            'epsilon', 0.0001, 'maxeval', 3000, ... 
+            'xlower', xplower, 'xupper', xpupper, ...
+            'save-freq', 10, 'restart-file', 'bfodisc.restart', ...
+            'verbosity', 'low'); %, ...
+            %'f-call-type', 'with-bound', ...
+            %'f-bound', 1.0)
+ 
+%% Save output
+    xstar_bfodisc(run).x = x;
+    xstar_bfodisc(run).fx = fx;
+    xstar_bfodisc(run).neval = neval;
+end
  
